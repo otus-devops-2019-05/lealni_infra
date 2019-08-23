7209265a57065170e738ddf218fc0226653d7146
Aleks Led <puhlyypups@gmail.com>
2019-08-23 19:53:49 +0300

    reddit app

---
+++ setup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+ruby -v
+bundler -v
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
